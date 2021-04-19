20b4ced4af901e70ec89e143fcb06201c0cdc393
jhonjoao <joao.jg237@gmail.com>
2021-04-19 01:20:08 -0300

    separate front for up

--- the-meal-api/src/services/getAllMealCategories.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-async function run(){
-    const categories = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php')
-
-    return categories;
-}
-
-module.exports = { run };
